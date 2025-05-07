63eaffdb6ff9ae5cc718102daaf9523b7fcac7af
Amjad-07 <146514436+Amjad-07@users.noreply.github.com>
2025-05-07 16:47:27 +0530

    Update app.py name

--- app.py
+++ app.py
@@ -3,11 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return """Hello, World! from Amjad  {
-  "db_user": "vamsi",
-  "db_pass": "securepassword123",
-  "api_key": "your-api-key-value"
-} """
+    return "Hello, World! from Amjad"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
